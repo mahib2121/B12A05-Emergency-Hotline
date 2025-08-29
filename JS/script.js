ae021899b679df049f859c57f25a8585afb8335d
mahib2121 <khanmahibalam@gmail.com>
2025-08-29 20:23:23 +0600

    heart icon implemented

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,60 @@
-console.log("Hello worild ")
+const increment = document.getElementById("heart_BTN")
+let conut = 0;
+increment.onclick = function () {
+    conut++;
+    defaultCnt.textContent = conut;
+}
+const increment1 = document.getElementById("heart_BTN1")
+increment1.onclick = function () {
+    conut++;
+    defaultCnt.textContent = conut;
+}
+
+const increment2 = document.getElementById("heart_BTN2")
+increment2.onclick = function () {
+    conut++;
+    defaultCnt.textContent = conut;
+}
+
+const increment3 = document.getElementById("heart_BTN3")
+increment3.onclick = function () {
+    conut++;
+    defaultCnt.textContent = conut;
+}
+const increment4 = document.getElementById("heart_BTN4")
+increment4.onclick = function () {
+    conut++;
+    defaultCnt.textContent = conut;
+}
+const increment5 = document.getElementById("heart_BTN5")
+increment5.onclick = function () {
+    conut++;
+    defaultCnt.textContent = conut;
+}
+
+const increment6 = document.getElementById("heart_BTN6")
+increment6.onclick = function () {
+    conut++;
+    defaultCnt.textContent = conut;
+}
+const increment7 = document.getElementById("heart_BTN7")
+increment7.onclick = function () {
+    conut++;
+    defaultCnt.textContent = conut;
+}
+const increment8 = document.getElementById("heart_BTN8")
+increment8.onclick = function () {
+    conut++;
+    defaultCnt.textContent = conut;
+}
+
+
+
+
+
+
+
+
+
+
+
